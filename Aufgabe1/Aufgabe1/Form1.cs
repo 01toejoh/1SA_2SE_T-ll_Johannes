1106ca4c3616838eaf43f43bbb32964f0b3c69e2
Schularbeit17 <Schularbeit17@rg-me.schule.suedtirol.it>
2018-03-06 12:16:22 +0100

    Funktion Farben

--- Aufgabe1/Aufgabe1/Form1.cs
+++ Aufgabe1/Aufgabe1/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿//Aufgabe2 Töll Johannes 3BEL
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -18,39 +19,76 @@ namespace Aufgabe1
 
         private void btm_erstellen_Click(object sender, EventArgs e)
         {
-            Auto Neuauto = new Auto();
+            //Auto Neuauto = new Auto();
+            Marke();
         }
 
-        public int Marke()
-        {
-            string rück;
-            if (cmb_Marke.Text == "BMW")
+
+            public void Marke()
             {
-                rück = "BMW";
+                
+                if (cmb_Marke.Text == "BMW")
+                {
+                    rtxt_Ausgabe.Text = "BMW";
+                }
+                else if (cmb_Marke.Text == "Porsche")
+                {
+                    rtxt_Ausgabe.Text = "Porsche";
+                }
+                else if (cmb_Marke.Text == "Ferrari")
+                {
+                    rtxt_Ausgabe.Text = "Ferrari";
+                }
+                else if (cmb_Marke.Text == "VW")
+                {
+                    rtxt_Ausgabe.Text = "VW";
+                }
+                else
+                {
+                    rtxt_Ausgabe.Text = "Audi";
+                }
+                
             }
-            else if (cmb_Marke.Text == "Porsche")
+            public void Farbe()
             {
-                rück = "Porsche";
+                if (cmb_Marke.Text == "Braun")
+                {
+                    rtxt_Ausgabe.Text = "Braun";
+                }
+                else if (cmb_Marke.Text == "Rot")
+                {
+                    rtxt_Ausgabe.Text = "Rot";
+                }
+                else if (cmb_Marke.Text == "Weiß")
+                {
+                    rtxt_Ausgabe.Text = "Weiß";
+                }
+                else if (cmb_Marke.Text == "Schwarz")
+                {
+                    rtxt_Ausgabe.Text = "Schwarz";
+                }
+                else if (cmb_Marke.Text == "Grün")
+                {
+                    rtxt_Ausgabe.Text = "Grün";
+                }
+                else if (cmb_Marke.Text == "Blau")
+                {
+                    rtxt_Ausgabe.Text = "Blau";
+                }
+                else if (cmb_Marke.Text == "Gelb")
+                {
+                    rtxt_Ausgabe.Text = "Gelb";
+                }
+                else if (cmb_Marke.Text == "Grau")
+                {
+                    rtxt_Ausgabe.Text = "Grau";
+                }
+                else
+                {
+                    rtxt_Ausgabe.Text = "Audi";
+                }
+                
             }
-            else if (cmb_Marke.Text == "Ferrari")
-            {
-                rück = "Ferrari";
-            }
-            else if (cmb_Marke.Text == "VW")
-            {
-                rück = "VW";
-            }
-            else
-            {
-                rück = "Audi";
-            }
-            return Convert.ToInt32(rück);
-        }
-            
-        
-    }
-    class Auto
-    {
         
     }
     
